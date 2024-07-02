4398b745591c03f7fe5640e8847f45493edad52c
shourya-r <rajputshourya7@gmail.com>
2024-07-03 02:25:05 +0530

    main function takes only one argument and prints a starting message

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function main() {
+  // process.argv returns an array with all the CLI arguments
+  if (process.argv.length < 3) {
+    console.log("No website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("Provide only one URL");
+    process.exit(1);
+  }
+  const baseURL = process.argv[2];
+  console.log(`Starting crawl at ${baseURL}`);
+}
+
+main();
