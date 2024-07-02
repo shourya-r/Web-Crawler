ebcafddbf26e2ccfbce0785bf2a9ddb20690d276
shourya-r <rajputshourya7@gmail.com>
2024-07-02 16:29:48 +0530

    Added test for changing capitalised URL to lowercase and forstripping http protocol

--- crawl.test.js
+++ crawl.test.js
@@ -14,3 +14,17 @@ test("normalizeURL removes trailing slash", () => {
   const expected = "blog.boot.dev/path";
   expect(actual).toBe(expected);
 });
+
+test("normalizeURL changes capitals to lowercase ", () => {
+  const input = "https://bLog.bOOt.Dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toBe(expected);
+});
+
+test("normalizeURL strips protocol", () => {
+  const input = "http://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toBe(expected);
+});
